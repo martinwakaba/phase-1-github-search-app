b504307f3b938913815533610dab9e9637065374
MartinWakaba <martin.wakaba@student.moringaschool.com>
2023-10-18 15:06:59 +0300

    Lab Done

--- js/index.js
+++ js/index.js
@@ -1,59 +1,100 @@
-// fucntion for getting user
-function getUsers(){
-    const form = document.getElementById('github-form');
-    const userData = document.getElementById('search');
-    const userList = document.getElementById('useer-list');
+// // fucntion for getting user
+// function getUsers(){
+//     const form = document.getElementById('github-form');
+//     const userData = document.getElementById('search');
+//     const userList = document.getElementById('user-list');
 
-    form.addEventListener('submit', (e) => {
-        e.preventDefault();
-        const username = userData.value;
+//     form.addEventListener('submit', (e) => {
+//         e.preventDefault();
+//         const username = userData.value;
 
-        if(username === ''){
-            alert('No input!!! PLEASE ENTER A USER NAME')
-            return;
-        }else{
-          let usernameSearch =   fetch(`https://api.github.com/users/${username}`)
-            .then((res) => res.json)
-            .then((obj) => console.log(obj))
+//         if(username === ''){
+//             alert('No input!!! PLEASE ENTER A USER NAME')
+//             return;
+//         }else{
+//           let usernameSearch =   fetch(`https://api.github.com/search/users?q=`)
+//             .then((res) => res.json)
+//             .then((obj) => console.log(obj))
             
-            let repoSearch = fetch(`https://api.github.com/repos/${username}`)
-            .then((res) => res.json)
-            .then((obj) => console.log(obj))
+//             let repoSearch = fetch(`https://api.github.com/users/octocat/repos`)
+//             .then((res) => res.json)
+//             .then((obj) => console.log(obj))
 
-            if(repoSearch === ''){
-                alert('No repo Found!!!')
-                return
-            }
-        }
+//             if(repoSearch === ''){
+//                 alert('No repo Found!!!')
+//                 return
+//             }
+//         }
 
-        userList.innerHTML = '<h1>Hi</h1>'
+//         userList.innerHTML = ''
 
-        const userListDetails = document.createElement('li');
+//         const userListDetails = document.createElement('li');
 
-        userListDetails.innerHTML = `<img src = ${userData.avatar_url} alt = ${userData.login}/>
-        <h3> ${userData.login}</h3>
-        <a href= ${userData.html_url} target =' _blank'  >View Profile</a>`;
+//         userListDetails.innerHTML = `<img src = ${userData.avatar_url} alt = ${userData.login}/>
+//         <h3> ${userData.login}</h3>
+//         <a href= ${userData.html_url} target =' _blank'  >View Profile</a>`;
 
-        userList.appendChild(userListDetails);
+//         userList.appendChild(userListDetails);
 
-        const repoList = document.getElementById('repo-List');
-        repoList.innerHTML = '';
+//         const repoList = document.getElementById('repo-List');
+//         repoList.innerHTML = '';
 
-        repoSearch.forEach((repo => {
-            const repoInfo = document.createElement('li');
-            repoInfo.innerHTML = `
-            <h3>${repo.name}</h3>
-             <p><strong>Description: </strong>${repo.description}</p>
-             <a href = '${repo.html_url}' target = '_blank'>View Repo</a>
-            `;
-            repoList.appendChild(repoInfo);
-        }
-        ))
+//         repoSearch.forEach((repo => {
+//             const repoInfo = document.createElement('li');
+//             repoInfo.innerHTML = `
+//             <h3>${repo.name}</h3>
+//              <p><strong>Description: </strong>${repo.description}</p>
+//              <a href = '${repo.html_url}' target = '_blank'>View Repo</a>
+//             `;
+//             repoList.appendChild(repoInfo);
+//         }
+//         ))
 
 
 
         
-    })
+//     })
+// }
+
+// document.addEventListener('DOMContentLoaded',getUsers);
+
+
+document.addEventListener('DOMContentLoaded',(e)=>{
+    const searchbx = document.querySelector('#search');
+    const searchbtn = document.querySelector('#submit');
+    const userApi = 'https://api.github.com/search/users?q=';
+    const repoApi = 'https://api.github.com/users/octocat/repos';
+  
+
+
+    async function checkUser(user){
+    const response1= await fetch(userApi + user)
+    const data = await response1.json()
+    const userlist=document.querySelector(`#user-list`)
+    userlist.innerHTML=``
+    user.forEach((item) => {
+        const userName = document.createElement(`li`)
+        userName.textContent= item.login
+        userlist.appendChild(userName)
+        
+    });
 }
 
-document.addEventListener('DOMContentLoaded',getUsers);
+    async function checkRepo(){
+        const response2 = await fetch(repoApi)
+        const data = await response2.json()
+        const repolist = document.querySelector(`#repos-list`)
+        repolist.innerHTML=``
+        data.forEach((item)=>{
+            const repoName = document.createElement(`li`)
+            repoName.textContent=item.name;
+            repolist.appendChild(repoName)
+        })
+}
+searchbtn.addEventListener(`click`,(e)=>{
+    e.preventDefault()
+    let userInsert = searchbx.value
+    checkRepo(userInsert)
+    checkUser(userInsert)
+})
+})
